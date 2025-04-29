253a1fb69af6dfe29bc314052dfa4bf8fe98e1a3
MaluCortez <86375623+MaluCortez@users.noreply.github.com>
2025-04-29 16:51:38 -0300

    Update Aula14.py

--- Aula14.py
+++ Aula14.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 configurar git:
-git config --global user.name *MAaluCortez*
+git config --global user.name *MaluCortez*
 git config --global user.email *00314633@ufrgs.br* 
